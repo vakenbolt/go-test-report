2e7b1d324b90bfdb01f127ef53c7ce2db3afdf27
Samuel A <desk.samuel.a@gmail.com>
2020-05-09 10:25:32 -0500

    Some code cleanup and updated tests

--- test_report.js
+++ test_report.js
@@ -6,6 +6,7 @@
  * @property {Array.<string>} Output
  * @property {boolean} Passed
  */
+class TestStatus {}
 
 /**
  * @typedef TestGroupData
@@ -13,17 +14,20 @@
  * @property {string} FailureIndicator
  * @property {Array.<TestStatus>}
  */
+class TestGroupData {}
 
 /**
  * @typedef TestResults
  * @type {Array.<TestGroupData>}
  */
+class TestResults extends Array {}
 
 /**
  * @typedef SelectedItems
  * @property {HTMLElement|EventTarget} testResults
  * @property {String} selectedTestGroupColor
  */
+class SelectedItems {}
 
 /**
  * @typedef GoTestReportElements
@@ -31,122 +35,134 @@
  * @property {HTMLElement} testResultsElem
  * @property {HTMLElement} testGroupListElem
  */
+class GoTestReportElements {}
 
 
 /**
  *
+ * @param {GoTestReportElements} elements
+ * @returns {{testResultsClickHandler: testResultsClickHandler}}
+ * @constructor
  */
-class GoTestReport {
-  /**
-   * @param {GoTestReportElements} elements
-   */
-  constructor(elements) {
-    const /**@type {SelectedItems}*/ selectedItems = {
-      testResults: null,
-      selectedTestGroupColor: null
-    }
-    elements.testResultsElem
-            .addEventListener('click', event => this.testResultsClickHandler(/**@type {HTMLElement}*/ event.target,
-                                                                             event.shiftKey,
-                                                                             elements.data,
-                                                                             selectedItems,
-                                                                             this.testGroupListHandler));
-
-    elements.testGroupListElem
-            .addEventListener('click', event => this.testGroupListHandler(/**@type {Element}*/ event.target,
-                                                                          elements.data));
+window.GoTestReport = function (elements) {
+  const /**@type {SelectedItems}*/ selectedItems = {
+    testResults: null,
+    selectedTestGroupColor: null
   }
 
-  /**
-   *
-   * @param {HTMLElement} target
-   * @param {boolean} shiftKey
-   * @param {TestResults} data
-   * @param {SelectedItems} selectedItems
-   * @param {function(target: Element, data: TestResults)} testGroupListHandler
-   */
-  testResultsClickHandler(target,
-                          shiftKey,
-                          data,
-                          selectedItems,
-                          testGroupListHandler) {
-    if (selectedItems.testResults != null) {
-      let f = /**@type {HTMLElement}*/ selectedItems.testResults;
-      f.style.backgroundColor = selectedItems.selectedTestGroupColor;
+  function addEventData(event) {
+    if (event.data == null) {
+      event.data = {target: event.target}
     }
-    const testGroupId = /**@type {number}*/ target['id'];
-    const testResults = /**@type {TestResults}*/ data[testGroupId]['TestResults'];
-    let testGroupList = /**@type {string}*/ '';
-    selectedItems.selectedTestGroupColor = getComputedStyle(target).getPropertyValue('background-color');
-    selectedItems.testResults = target;
-    target.style.backgroundColor = 'black';
-    for (let i = 0; i < testResults.length; i++) {
-      const testResult = /**@type {TestGroupData}*/ testResults[i];
-      const testPassed = /**@type {boolean}*/ testResult.Passed
-      const testPassedStatus = /**@type {string}*/ (testPassed) ? '' : 'failed';
-      const testId = /**@type {string}*/ target.attributes['id'].value;
-      testGroupList += `<div class="testGroupRow ${testPassedStatus}" data-testid="${testId}" data-index="${i}">
+    return event
+  }
+
+
+  const goTestReport = {
+    /**
+     *
+     * @param {HTMLElement} target
+     * @param {boolean} shiftKey
+     * @param {TestResults} data
+     * @param {SelectedItems} selectedItems
+     * @param {function(target: Element, data: TestResults)} testGroupListHandler
+     */
+    testResultsClickHandler: function (target,
+                                       shiftKey,
+                                       data,
+                                       selectedItems,
+                                       testGroupListHandler) {
+      if (selectedItems.testResults != null) {
+        let f = /**@type {HTMLElement}*/ selectedItems.testResults
+        f.style.backgroundColor = selectedItems.selectedTestGroupColor
+      }
+      const testGroupId = /**@type {number}*/ target.id
+      const testResults = /**@type {TestResults}*/ data[testGroupId]['TestResults']
+      let testGroupList = /**@type {string}*/ ''
+      selectedItems.selectedTestGroupColor = getComputedStyle(target).getPropertyValue('background-color')
+      selectedItems.testResults = target
+      target.style.backgroundColor = 'black'
+      for (let i = 0; i < testResults.length; i++) {
+        const testResult = /**@type {TestGroupData}*/ testResults[i]
+        const testPassed = /**@type {boolean}*/ testResult.Passed
+        const testPassedStatus = /**@type {string}*/ (testPassed) ? '' : 'failed'
+        const testId = /**@type {string}*/ target.attributes['id'].value
+        testGroupList += `<div class="testGroupRow ${testPassedStatus}" data-testid="${testId}" data-index="${i}">
         <span class="testStatus ${testPassedStatus}">${(testPassed) ? '&check' : '&cross'};</span>
         <span class="testTitle">${testResult.TestName}</span>
         <span class="testDuration"><span>${testResult.ElapsedTime}s </span>⏱</span>
       </div>`
-    }
-    const testGroupListElem = document.querySelector('div.cardContainer.testGroupList');
-    testGroupListElem.innerHTML = '';
-    testGroupListElem.innerHTML = testGroupList;
+      }
+      const testGroupListElem = elements.testGroupListElem
+      testGroupListElem.innerHTML = ''
+      testGroupListElem.innerHTML = testGroupList
 
-    if (shiftKey) {
-      testGroupListElem.querySelectorAll('.testGroupRow')
-                       .forEach((elem) => testGroupListHandler(elem, data))
-    } else if (testResults.length === 1) {
-      testGroupListHandler(testGroupListElem.querySelector('.testGroupRow'), data)
-    }
-  }
+      if (shiftKey) {
+        testGroupListElem.querySelectorAll('.testGroupRow')
+                         .forEach((elem) => testGroupListHandler(elem, data))
+      } else if (testResults.length === 1) {
+        testGroupListHandler(testGroupListElem.querySelector('.testGroupRow'), data)
+      }
+    },
 
-  /**
-   *
-   * @param {Element} target
-   * @param {TestResults} data
-   */
-  testGroupListHandler(target, data) {
-    const attribs = target['attributes']
-    if (attribs.hasOwnProperty('data-testid')) {
-      const testId = /**@type {number}*/ attribs['data-testid'].value;
-      const index = /**@type {number}*/ attribs['data-index'].value
-      const testStatus = /**@type {TestStatus}*/ data[testId]['TestResults'][index]
-      const testOutputDiv = /**@type {HTMLDivElement}*/ target.querySelector('div.testOutput');
+    /**
+     *
+     * @param {Element} target
+     * @param {TestResults} data
+     */
+    testGroupListHandler: function (target, data) {
+      const attribs = target['attributes']
+      if (attribs.hasOwnProperty('data-testid')) {
+        const testId = /**@type {number}*/ attribs['data-testid'].value
+        const index = /**@type {number}*/ attribs['data-index'].value
+        const testStatus = /**@type {TestStatus}*/ data[testId]['TestResults'][index]
+        const testOutputDiv = /**@type {HTMLDivElement}*/ target.querySelector('div.testOutput')
 
-      if (testOutputDiv == null) {
-        const testOutputDiv = document.createElement('div');
-        testOutputDiv.classList.add('testOutput');
-        const consoleSpan = document.createElement('span');
-        consoleSpan.classList.add('console');
-        const testDetailDiv = document.createElement('div');
-        testDetailDiv.classList.add('testDetail');
-        const packageNameDiv = document.createElement('div');
-        packageNameDiv.classList.add('package')
-        packageNameDiv.innerHTML = `<strong>Package:</strong> ${testStatus.Package}`;
-        const testFileNameDiv = document.createElement('div');
-        testFileNameDiv.classList.add('filename')
-        testFileNameDiv.innerHTML = `<strong>Filename:</strong> `;
+        if (testOutputDiv == null) {
+          const testOutputDiv = document.createElement('div')
+          testOutputDiv.classList.add('testOutput')
+          const consoleSpan = document.createElement('span')
+          consoleSpan.classList.add('console')
+          const testDetailDiv = document.createElement('div')
+          testDetailDiv.classList.add('testDetail')
+          const packageNameDiv = document.createElement('div')
+          packageNameDiv.classList.add('package')
+          packageNameDiv.innerHTML = `<strong>Package:</strong> ${testStatus.Package}`
+          const testFileNameDiv = document.createElement('div')
+          testFileNameDiv.classList.add('filename')
+          testFileNameDiv.innerHTML = `<strong>Filename:</strong> `
 
-        testDetailDiv.insertAdjacentElement('beforeend', packageNameDiv);
-        testDetailDiv.insertAdjacentElement('beforeend', testFileNameDiv);
-        testOutputDiv.insertAdjacentElement('afterbegin', consoleSpan);
-        testOutputDiv.insertAdjacentElement('beforeend', testDetailDiv);
-        target.insertAdjacentElement('beforeend', testOutputDiv);
+          testDetailDiv.insertAdjacentElement('beforeend', packageNameDiv)
+          testDetailDiv.insertAdjacentElement('beforeend', testFileNameDiv)
+          testOutputDiv.insertAdjacentElement('afterbegin', consoleSpan)
+          testOutputDiv.insertAdjacentElement('beforeend', testDetailDiv)
+          target.insertAdjacentElement('beforeend', testOutputDiv)
 
-        if (testStatus.Passed) {
-          consoleSpan.classList.remove('failed');
+          if (testStatus.Passed) {
+            consoleSpan.classList.remove('failed')
+          } else {
+            consoleSpan.classList.add('failed')
+          }
+          consoleSpan.textContent = testStatus.Output.join('')
         } else {
-          consoleSpan.classList.add('failed');
+          testOutputDiv.remove()
         }
-        consoleSpan.textContent = testStatus.Output.join('');
-      } else {
-        testOutputDiv.remove()
       }
     }
   }
-}
 
-window.GoTestReport = GoTestReport;
+  elements.testResultsElem
+          .addEventListener('click', event =>
+            goTestReport.testResultsClickHandler(/**@type {HTMLElement}*/ addEventData(event).data.target,
+                                           event.shiftKey,
+                                           elements.data,
+                                           selectedItems,
+                                           goTestReport.testGroupListHandler))
+
+  elements.testGroupListElem
+          .addEventListener('click', event =>
+            goTestReport.testGroupListHandler(/**@type {Element}*/ event.target,
+                                        elements.data))
+
+  return goTestReport
+}
--- test_report.test.js
+++ test_report.test.js
@@ -1,24 +1,59 @@
 const expect = require('@jest/globals').expect
 require('./test_report.js')
 
-const mockData = [{
-  "TestResults": [{
-    TestName: "my_sample_test",
-    Package: "test/package",
-    Output: [
-      "test output 1\n",
-      "test output 2\n",
-      "test output 3\n",
-    ]
+/**
+ * @property {Array.<TestResults>} TestResults
+ */
+const mockData = [
+  {
+    "TestResults": [{
+      TestName: "my_sample_test 1",
+      Package: "test/package 1",
+      Output: [
+        "test output A 1\n",
+        "test output A 2\n",
+        "test output A 3\n",
+      ]
+    }]
+  }, {
+    "TestResults": [{
+      TestName: "my_sample_test 2",
+      Package: "test/package 2",
+      Output: [
+        "test output B 1\n",
+        "test output B 2\n",
+        "test output B 3\n",
+      ]
+    }]
+  }, {
+    "TestResults": [{
+      TestName: "my_sample_test 3",
+      Package: "test/package 3",
+      Output: [
+        "test output C 1\n",
+        "test output C 2\n",
+        "test output C 3\n",
+      ]
+    }]
   }]
-}]
 
 
 function createTestElements() {
   const testResultsElem = document.createElement('div')
   testResultsElem.id = 'testResults'
   const testGroupListElem = document.createElement('div')
+  testGroupListElem.classList.add('cardContainer')
+  testGroupListElem.classList.add('testGroupList')
   testGroupListElem.id = 'testGroupList'
+
+  let counter = 0
+  mockData.forEach((_) => {
+    let testResultGroup = document.createElement('div')
+    testResultGroup.id = counter.toString()
+    counter += 1
+    testResultsElem.insertAdjacentElement("beforeend", testResultGroup)
+  })
+
   return {
     data: mockData,
     testResultsElem: testResultsElem,
@@ -26,16 +61,17 @@ function createTestElements() {
   }
 }
 
-test('test GoTestReport constructor', () => {
+test('test GoTestReport constructor with click event on a test group', () => {
   const testElements = createTestElements()
-  const goTestReport = new window.GoTestReport(testElements);
+  const goTestReport = window.GoTestReport(testElements);
   const invocationCounts = {testResultsClickHandler: 0}
   goTestReport.testResultsClickHandler = function (target,
                                                    shiftKey,
                                                    data,
                                                    selectedItems,
                                                    testGroupListHandler) {
-    expect(target).toBe(testElements.testResultsElem)
+    expect(true).toBe(true)
+    expect(target.outerHTML).toBe(`<div id="0"></div>`)
     expect(shiftKey).toBe(false)
     expect(data).toBe(mockData)
     expect(selectedItems.testResults).toBeNull()
@@ -43,9 +79,13 @@ test('test GoTestReport constructor', () => {
     expect(testGroupListHandler).toBe(goTestReport.testGroupListHandler)
     invocationCounts.testResultsClickHandler += 1
   }
-  testElements.testResultsElem.dispatchEvent(new MouseEvent('click'))
+  const clickEvent = new MouseEvent('click')
+  clickEvent.data = {
+    target: testElements.testResultsElem.querySelector('#\\30')
+  }
+  testElements.testResultsElem.dispatchEvent(clickEvent)
   expect(invocationCounts.testResultsClickHandler).toBe(1)
-});
+})
 
 test('test testGroupListHandler', () => {
   const goTestReport = new window.GoTestReport(createTestElements());
@@ -62,16 +102,16 @@ test('test testGroupListHandler', () => {
 
   const consoleElem = testOutputDiv.querySelector('.console.failed')
   expect(consoleElem).toBeDefined()
-  expect(consoleElem.textContent).toBe('test output 1\ntest output 2\ntest output 3\n')
+  expect(consoleElem.textContent).toBe('test output A 1\ntest output A 2\ntest output A 3\n')
 
   const testDetailElem = testOutputDiv.querySelector('.testDetail')
   expect(testDetailElem).toBeDefined()
 
   const packageElem = testDetailElem.querySelector('.package')
   expect(packageElem).toBeDefined()
-  expect(packageElem.innerHTML).toBe(`<strong>Package:</strong> test/package`)
+  expect(packageElem.innerHTML).toBe(`<strong>Package:</strong> test/package 1`)
 
   const filenameElem = testDetailElem.querySelector('.filename')
   expect(filenameElem).toBeDefined()
   expect(filenameElem.innerHTML).toBe(`<strong>Filename:</strong> `)
-});
+})
